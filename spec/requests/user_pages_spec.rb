94d2acb5d59d42e2331236b8212a412ac3129ade
OsadaMac <Osadake.212@gmail.com>
2014-05-06 16:16:03 +0900

    create user controller

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  describe "signup page" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit signup_path
+    end
+  end
+end
