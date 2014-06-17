050bcb14c43730e4d1d1f80d786506a2d4723ee4
OsadaTakuya <Osada@localhost.localdomain>
2014-06-17 11:25:34 +0900

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	def new 
+		render 'new'
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# sign in user and show user page
+			sign_in user
+			redirect_to user
+		else
+			# show error message and render sign in form
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+end
